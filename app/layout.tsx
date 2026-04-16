5919a74b83bd1809d3ec47c4a3cd5cbb5b640d57
Valeriia <valeriia.kravchuk.ua@gmail.com>
2026-04-16 17:08:13 +0300

    add fix 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({
                                        children,
                                    }: Readonly<{
     children: React.ReactNode;
-    modal: React.ReactNode;
 }>) {
 
     return (
